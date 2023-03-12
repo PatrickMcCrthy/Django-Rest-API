5af58d8b08b6bd4c3cc2ee5a9acc4d9e2ed6af67
patrick <mccarthy889@gmail.com>
2023-03-12 22:27:02 +0000

    initial commit

--- Desktop/fyp_git/MartPlus/marts/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-from django.db import models
-
-class Marts(models.Model):
-  name = models.CharField(max_length=255)
-  address = models.CharField(max_length=255)
--- Desktop/fyp_git/MartPlus/marts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # path('marts/', views.marts, name='marts'),
-    # path('skibbereen/', views.skibbereen, name='skibbereen')
-]
